db6fadd36be79a539704e3074c64cbc88ab86c67
ryan <ryanvaniderstine@maximus.com>
2015-11-02 13:00:19 -0400

    Improvement to volume clean. Fix preference for ipv4. Fix exhausted /dev/random usage.

--- docker-clean-volumes.sh
+++ docker-clean-volumes.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 if [ "$1" != "--force" ]; then
-  OPTS=-h
-  echo Cleanup docker volumes: remove unused volumes.
+  OPTS=--dry-run
   echo Usage: docker-cleanup-volumes.sh [--force] [--dry-run] [--verbose]
-  echo   --force:       perform the actual deletion.
-  echo   -n, --dry-run: dry run: display what would get removed.
-  echo   -v, --verbose: verbose output.
-  exit 1
 fi
 IMAGE=martin/docker-cleanup-volumes
 SOCKPATH=/var/run/docker.sock
